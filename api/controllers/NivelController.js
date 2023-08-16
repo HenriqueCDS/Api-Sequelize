cfc77d7545f9a0f83daf22507002e9bf2b49f1c7
henrique <henriquecordeiro054@gmail.com>
2023-08-15 23:54:14 -0300

    Update aula 6

--- api/controllers/NivelController.js
+++ api/controllers/NivelController.js
@@ -10,5 +10,53 @@ class NivelController {
         return res.status(500).json(error.message);
       }
     }
+    static async pegaUmNivel(req, res) {
+      const { id } = req.params
+      try {
+        const umaNiveis = await database.Niveis.findOne( { 
+          where: { 
+            id: Number(id) 
+          }
+        })
+        return res.status(200).json(umaNiveis)
+      } catch (error) {
+        return res.status(500).json(error.message)
+      }
+    }
+  
+    static async criaNivel(req, res) {
+      const novaNiveis = req.body
+      try {
+        const novaNiveisCriada = await database.Niveis.create(novaNiveis)
+        return res.status(200).json(novaNiveisCriada)
+      } catch (error) {
+        return res.status(500).json(error.message)
+      }
+    }
+  
+    static async atualizaNivel(req, res) {
+      const { id } = req.params
+      const novasInfos = req.body
+      try {
+        await database.Niveis.update(novasInfos, { where: { id: Number(id) }})
+        const NiveisAtualizada = await database.Niveis.findOne( { where: { id: Number(id) }})
+        return res.status(200).json(NiveisAtualizada)
+      } catch (error) {
+        return res.status(500).json(error.message)
+      }
+    }
+  
+    //deletar um registro
+    static async apagaNivel(req, res) {
+      const { id } = req.params
+      try {
+        await database.Niveis.destroy({ where: { id: Number(id) }})
+        return res.status(200).json({ mensagem: `id ${id} deletado` })
+  
+      } catch (error) {
+        return res.status(500).json(error.message)
+      }
+    }
+  
 }
 module.exports = NivelController
--- api/routes/niveisRoute.js
+++ api/routes/niveisRoute.js
@@ -3,7 +3,12 @@ const NiveisController = require('../controllers/NivelController')
 
 const router = Router()
 
+
 router.get('/niveis', NiveisController.pegaTodosOsNiveis);
+router.get('/niveis/:id', NiveisController.pegaUmNivel);
+router.post('/niveis', NiveisController.criaNivel);
+router.patch('/niveis/:id', NiveisController.atualizaNivel);
+router.delete('/niveis/:id', NiveisController.apagaNivel);
 
 
 module.exports = router
