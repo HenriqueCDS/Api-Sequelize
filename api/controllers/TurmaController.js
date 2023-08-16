2de03d15e0b74ef604291fe1f7d7379f4d42e9d0
henrique <henriquecordeiro054@gmail.com>
2023-08-15 23:53:18 -0300

    update aula routes and controlers aula 6

--- api/controllers/TurmaController.js
+++ api/controllers/TurmaController.js
@@ -10,5 +10,54 @@ class TurmaController {
         return res.status(500).json(error.message);
       }
     }
+    static async pegaUmaTurma(req, res) {
+      const { id } = req.params
+      try {
+        const umaTurmas = await database.Turmas.findOne( { 
+          where: { 
+            id: Number(id) 
+          }
+        })
+        return res.status(200).json(umaTurmas)
+      } catch (error) {
+        return res.status(500).json(error.message)
+      }
+    }
+  
+    static async criaTurma(req, res) {
+      const novaTurmas = req.body
+      try {
+        const novaTurmasCriada = await database.Turmas.create(novaTurmas)
+        return res.status(200).json(novaTurmasCriada)
+      } catch (error) {
+        return res.status(500).json(error.message)
+      }
+    }
+  
+    static async atualizaTurma(req, res) {
+      const { id } = req.params
+      const novasInfos = req.body
+      try {
+        await database.Turmas.update(novasInfos, { where: { id: Number(id) }})
+        const TurmasAtualizada = await database.Turmas.findOne( { where: { id: Number(id) }})
+        return res.status(200).json(TurmasAtualizada)
+      } catch (error) {
+        return res.status(500).json(error.message)
+      }
+    }
+  
+    //deletar um registro
+    static async apagaTurma(req, res) {
+      const { id } = req.params
+      try {
+        await database.Turmas.destroy({ where: { id: Number(id) }})
+        return res.status(200).json({ mensagem: `id ${id} deletado` })
+  
+      } catch (error) {
+        return res.status(500).json(error.message)
+      }
+    }
+  
+  
 }
 module.exports = TurmaController;
